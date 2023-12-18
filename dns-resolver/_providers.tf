5292405439539073d064d943f38e33bf9352270d
Michael Lacore <michael@massdriver.cloud>
2023-12-18 09:17:11 -0800

    fixing cidr collision issue (#3)
    * fixing cidr collision issue
    
    * move comment

---
+++ dns-resolver/_providers.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    massdriver = {
+      source  = "massdriver-cloud/massdriver"
+      version = "~> 1.0"
+    }
+    utility = {
+      source = "massdriver-cloud/utility"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+    jq = {
+      source  = "massdriver-cloud/jq"
+      version = "~> 0.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+
+  client_id       = var.azure_service_principal.data.client_id
+  tenant_id       = var.azure_service_principal.data.tenant_id
+  client_secret   = var.azure_service_principal.data.client_secret
+  subscription_id = var.azure_service_principal.data.subscription_id
+}
--- dns-resolver/dns_resolver.tf
+++ dns-resolver/dns_resolver.tf
@@ -1,10 +1,32 @@
+# DNS resolver is required as this is the component that resolves DNS requests.
+# Without this, only way to connect to resources is using private IP address.
+# https://learn.microsoft.com/en-us/azure/architecture/example-scenario/networking/azure-dns-private-resolver
+
+# Multi-step build is required as there are 2 subnets provisioning in this bundle, and single step causes CIDR collision.
+
+locals {
+  split_vnet_id       = split("/", var.azure_virtual_network.data.infrastructure.id)
+  vnet_name           = element(local.split_vnet_id, length(local.split_vnet_id) - 1)
+  vnet_resource_group = element(local.split_vnet_id, index(local.split_vnet_id, "resourceGroups") + 1)
+}
+
+data "azurerm_virtual_network" "lookup" {
+  name                = local.vnet_name
+  resource_group_name = local.vnet_resource_group
+}
+
+data "azurerm_subnet" "lookup" {
+  for_each             = toset(data.azurerm_virtual_network.lookup.subnets)
+  name                 = each.key
+  virtual_network_name = local.vnet_name
+  resource_group_name  = local.vnet_resource_group
+}
+
 resource "utility_available_cidr" "dns_resolver" {
   from_cidrs = data.azurerm_virtual_network.lookup.address_space
   used_cidrs = flatten([for subnet in data.azurerm_subnet.lookup : subnet.address_prefixes])
   mask       = 28
 }
-
-# DNS resolver is required as this is the component that resolves DNS requests. Without this, only way to connect to resources is using private IP address. https://learn.microsoft.com/en-us/azure/architecture/example-scenario/networking/azure-dns-private-resolver
 
 resource "azurerm_private_dns_resolver" "dns_resolver" {
   name                = var.md_metadata.name_prefix
@@ -34,6 +56,7 @@ resource "azurerm_private_dns_resolver_inbound_endpoint" "dns_resolver" {
   private_dns_resolver_id = azurerm_private_dns_resolver.dns_resolver.id
   location                = var.azure_virtual_network.specs.azure.region
   tags                    = var.md_metadata.default_tags
+
   ip_configurations {
     private_ip_allocation_method = "Dynamic"
     subnet_id                    = azurerm_subnet.dns_resolver.id
